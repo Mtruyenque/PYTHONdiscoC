f373732e75218e8df2311ea1d07afc814d1720d8
Mtruyenque <mtruyenque8843@gmail.com>
2024-08-16 22:18:54 +0200

    16/08practica

---
+++ CURSO1/strings_cadena.py
@@ -0,0 +1,9 @@
+nombreCurso= "ultimate Python"
+descripcionCurso = """ ultimate Python, estes curso contempla todos los detalles que necesitas aprender.""" 
+print( nombreCurso,descripcionCurso) 
+print(len(nombreCurso)) 
+print(nombreCurso [0])
+print(nombreCurso[0:8])
+print(nombreCurso[9:13])
+print(nombreCurso[:8])
+print(nombreCurso[ : ])
---
+++ CURSO1/variable.py
@@ -0,0 +1,8 @@
+Nombre_curso="ultimate python"
+NombreCurso= " hola "
+Nombrecurson = " funcionara"
+NomCurso = "estudia"
+print(Nombre_curso,NombreCurso,Nombrecurson, NomCurso )
+alumnos = 5000 
+puntaje = 9.9 
+publicado = True
